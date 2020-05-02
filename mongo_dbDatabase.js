ed453d126520f8db653603d7924364cb428f1b01
ugohuche <61969079+ugohuche@users.noreply.github.com>
2020-05-02 21:50:21 +0100

    Delete mongo_dbDatabase.js

--- mongo_dbDatabase.js
+++
@@ -1,9 +0,0 @@
-var mongo = require('mongodb');
-var mongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/ugohuche";
-
-mongoClient.connect(url, function(err, db){
-    if (err) throw err;
-    console.log("Database created by ugohuche");
-    db.close();
-})
